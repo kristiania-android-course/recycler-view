37627860eabae010049353dd0b4a938067b80a3c
Arun Pillai <arun.vijayan.pillai@shortcut.no>
2020-02-02 14:45:12 +0100

    adding resources

--- app/src/main/java/com/avp/farm_animals/AnimalActivity.kt
+++ app/src/main/java/com/avp/farm_animals/AnimalActivity.kt
@@ -1,19 +1,22 @@
 package com.avp.farm_animals
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AnimalActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_animal)
 
-        /*fimClick*/
+        /*fgetDrawable*/
+
+        /*fimgBigClick*/
     }
 
-    /*fopAnim*/
+    /*fplayAudio*/
 
 
-    /*fwhenOnClick*/
 }
--- app/src/main/java/com/avp/farm_animals/AnimalsListActivity.kt
+++ app/src/main/java/com/avp/farm_animals/AnimalsListActivity.kt
@@ -3,17 +3,10 @@ package com.avp.farm_animals
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AnimalsListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        /*fimClick*/
+        setContentView(R.layout.activity_animals_list)
     }
-
-    /*fopAnim*/
-
-
-    /*fwhenOnClick*/
 }
