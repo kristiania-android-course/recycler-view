270dc09c6403eec001ab20efa785ba83681c6b4d
Arun Pillai <arun.vijayan.pillai@shortcut.no>
2020-02-02 23:36:59 +0100

    adding resources

--- app/src/main/java/com/avp/farm_animals/AnimalsListActivity.kt
+++ app/src/main/java/com/avp/farm_animals/AnimalsListActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class AnimalsListActivity : AppCompatActivity() {
 
+    /*flistAnimals*/
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_animals_list)
